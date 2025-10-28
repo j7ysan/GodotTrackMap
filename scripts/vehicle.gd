30eb2ad6fb274530bb4e5358cabe2861d40279a6
kiki-sko <194424667+kiki-sko@users.noreply.github.com>
2025-10-28 03:02:02 -0700

    moved collision to track and tweaked vehicle !

--- scripts/vehicle.gd
+++ scripts/vehicle.gd
@@ -1,26 +1,44 @@
 extends VehicleBody3D
+@export var MAX_RPM = 1200
+@export var MAX_TORQUE = 800
+@export var TURN_SPEED = 1
+@export var TURN_AMOUNT = 0.3
+@export var BRAKE_AMOUNT = 20
+
+func _ready():
+	reset()
 
 func _physics_process(delta):
-	steering = move_toward(steering, Input.get_axis("right","left") * 0.6, delta * 1)
+	steering = move_toward(steering, Input.get_axis("right","left") * TURN_AMOUNT, delta * TURN_SPEED)
+	
+	var RPM = (abs($Wheel3.get_rpm()) + abs($Wheel4.get_rpm())) / 2.0
+	var torque = Input.get_axis("down","up") * MAX_TORQUE * (1.0 - RPM / MAX_RPM)
+	
 	if Input.get_axis("down","up") > 0:
 		if global_transform.basis.z.dot(linear_velocity) < -1:
-			brake = 3
+			brake = BRAKE_AMOUNT
 			engine_force = 0
 		else:
 			brake = 0
-			engine_force = 150
+			engine_force = torque
 	elif Input.get_axis("down","up") < 0:
 		if global_transform.basis.z.dot(linear_velocity) > 1:
-			brake = 3
+			brake = BRAKE_AMOUNT
 			engine_force = 0
 		else:
 			brake = 0
-			engine_force = -150
+			engine_force = torque
 	else:
-		brake = 0
+		brake = 0.25
 		engine_force = 0
+	
+	if position.y < 80:
+		reset()
+
+func reset():
+	rotation = Vector3.ZERO
+	position = Vector3(250,105,65)
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_accept"):
-		print("as")
-		rotation = Vector3(0,0,0)
+		reset()

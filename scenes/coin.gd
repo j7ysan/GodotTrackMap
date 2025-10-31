48ef352afe533b7abc9f299ba2625af6b498d85d
zoinotna <zoinotna0@gmail.com>
2025-10-30 19:10:35 -0700

    cleaned up code
    Fixed minor scripting syntax found by the debugger

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -3,16 +3,12 @@ extends Area3D
 const ROT_SPEED = 2 #degree of rotation per frame
 @export var coin_counter : CanvasLayer 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta):
 	rotate_y(deg_to_rad(ROT_SPEED))
 
 
-func _on_body_entered(body: Node3D) -> void:
+func _on_body_entered(_body):
 	Global.coins += 1
 	coin_counter.get_node("CoinsLabel").text = str(Global.coins)
 	set_collision_layer_value(3, false)
